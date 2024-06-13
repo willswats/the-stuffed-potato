b06d42c837c7bfb28819f3232eaebc2ca9094126
willswats <william@williamwatson.dev>
2024-06-13 01:30:51 +0100

    feat: add favicon and manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,24 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content="The Stuffed Potato" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="site.webmanifest" />
       </Head>
       <body>
         <Main />
