4fb3aecd7b9fd0595ff2c77de75b9866f059ce78
willswats <william@williamwatson.dev>
2024-12-06 12:38:35 +0000

    refactor: use next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+import "@/styles/globals.css";
+
+const roboto = Roboto({
+  weight: ["100", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  preload: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
